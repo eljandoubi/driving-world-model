d5bbe4f65026af315d4c0a9f77ae7d8b8e892946
eljandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-05-16 21:36:26 +0200

    try end to end model

--- data_prep.py
+++ data_prep.py
@@ -1,12 +1,31 @@
 from datasets import load_dataset
 from dotenv import load_dotenv
 
+# from tqdm import tqdm
+
 print("Loading environment variables...", load_dotenv())
 
-ds = load_dataset("immanuelpeter/carla-autopilot-multimodal-dataset", split="test")
+ds = load_dataset("immanuelpeter/carla-autopilot-multimodal-dataset", split="validation", streaming=True)
 
-ds = ds.sort(["run_id","frame"])
+# ds = ds.sort(["run_id","frame"])
+# runids = []
+# for d in tqdm(ds):
+#     if d["run_id"] not in runids:
+#         i=-1
+#         print("\nnew run",d["run_id"])
+#         runids.append(d["run_id"])
 
-print(ds)
+#     assert i<d["frame"], "data must be sorted"
+#     i = d["frame"]
 
-
+# print(runids)
+frames = {}
+for i,d in enumerate(ds):
+    if i==0:
+        frames["frame_t"]=d["image_front"]
+        frames["action"]=[d["throttle"],d["steer"],d["brake"]]
+    elif i==1:
+        frames["frame_tp1"]=d["image_front"]
+    else:
+        break
+print(frames)
--- model.py
+++ model.py
@@ -1,50 +1,74 @@
 import torch
 import torch.nn as nn
+from diffusers.models.attention import FeedForward
 from diffusers.models.unets.unet_2d_condition import UNet2DConditionModel
 
 
-class ActionEmbedder(nn.Module):
-    def __init__(self, action_dim:int=3, embed_dim:int=128,
-                 num_tokens:int=4, hidden_dim:int=128):
-        super().__init__()
+class ActionEmbedder(FeedForward):
+    def __init__(self, action_dim:int=3, embed_dim:int=1024,
+                 num_tokens:int=4, hidden_dim:int=1024,
+                 dropout:float=0.1, act_fn:str="swiglu"):
+        
+        super().__init__(dim=action_dim,  inner_dim=hidden_dim,
+                         dim_out=embed_dim * num_tokens,
+                         activation_fn=act_fn, dropout=dropout)
 
         self.num_tokens = num_tokens
 
-        self.mlp = nn.Sequential(
-            nn.Linear(action_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, embed_dim * num_tokens)
-        )
-
     def forward(self, a:torch.Tensor):
         # a: (B, action_dim)
-        x = self.mlp(a)  # (B, embed_dim * tokens)
+        x = super().forward(a)  # (B, embed_dim * tokens)
         x = x.view(a.shape[0], self.num_tokens, -1)
         return x  # (B, tokens, embed_dim)
     
 
 class WorldModel(nn.Module):
-    def __init__(self, base_name:str="google/ddpm-cifar10-32",
-                 action_dim:int=3, num_tokens:int=4, hidden_dim:int=128):
+    def __init__(self, base_name:str="runwayml/stable-diffusion-v1-5",
+                 action_dim:int=3, num_tokens:int=4, hidden_dim:int=1024):
         
         super().__init__()
         self.unet = UNet2DConditionModel.from_pretrained(
-            "google/ddpm-cifar10-32"
-        )
+                base_name,
+                subfolder="unet"
+            )
+        
         self.action_embedder = ActionEmbedder(action_dim=action_dim,
                                    embed_dim = self.unet.config.cross_attention_dim, # pyright: ignore[reportAttributeAccessIssue]
                                    num_tokens=num_tokens,
                                    hidden_dim=hidden_dim,
                                    )
         
-    def forward(self, a_t:torch.Tensor,noisy_x: torch.Tensor, t: torch.Tensor):
-        # 🔥 action embedding
+    def forward(self, a_t:torch.Tensor, x_t: torch.Tensor):
         cond = self.action_embedder(a_t)  # (B, tokens, dim)
-
-        # forward
-        noise_pred = self.unet(
-            sample=noisy_x,
+        B = x_t.shape[0]
+        t = torch.ones((B,),dtype=torch.long,device=x_t.device)
+        x_tp1_pred = self.unet(
+            sample=x_t,
             timestep=t,
             encoder_hidden_states=cond
         ).sample
-        return noise_pred
+        return x_tp1_pred
+    
+if __name__ == "__main__":
+    from torchvision import transforms
+    from tqdm import tqdm
+
+    from data_prep import frames
+    model = WorldModel().cuda()
+    im_s = model.unet.config.sample_size * 4
+    print(im_s)
+    in_c = model.unet.config.in_channels
+    transform = transforms.Compose([
+        transforms.Resize((im_s, im_s)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5]*in_c, std=[0.5]*in_c)
+    ])
+    x_t = transform(frames["frame_t"]).unsqueeze(0).cuda()
+    x_tp1 = transform(frames["frame_tp1"]).unsqueeze(0).cuda()
+    a_t = torch.tensor(frames["action"]).unsqueeze(0).cuda()
+    for _ in tqdm(range(1)):
+        with torch.inference_mode():
+            x_tp1_pred = model(a_t,x_t)
+            loss = nn.functional.mse_loss(x_tp1_pred,x_tp1)
+            print(loss)
+
