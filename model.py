26a7e27e5b3ee4235e0e838cd11e9b36a3ea1061
eljandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-05-16 19:33:51 +0200

    fix typos

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from diffusers import DDPMScheduler, UNet2DConditionModel
+from diffusers.models.unets.unet_2d_condition import UNet2DConditionModel
 
 
 class ActionEmbedder(nn.Module):
@@ -31,23 +31,20 @@ class WorldModel(nn.Module):
         self.unet = UNet2DConditionModel.from_pretrained(
             "google/ddpm-cifar10-32"
         )
-        self.cond = ActionEmbedder(action_dim=action_dim,
-                                   embed_dim = self.unet.config.cross_attention_dim,
+        self.action_embedder = ActionEmbedder(action_dim=action_dim,
+                                   embed_dim = self.unet.config.cross_attention_dim, # pyright: ignore[reportAttributeAccessIssue]
                                    num_tokens=num_tokens,
                                    hidden_dim=hidden_dim,
                                    )
         
-        def forward(self, a_t:torch.Tensor,noisy_x: torch.Tensor, t: torch.Tensor):
-            # 🔥 action embedding
-            cond = self.action_embedder(a_t)  # (B, tokens, dim)
+    def forward(self, a_t:torch.Tensor,noisy_x: torch.Tensor, t: torch.Tensor):
+        # 🔥 action embedding
+        cond = self.action_embedder(a_t)  # (B, tokens, dim)
 
-            # forward
-            noise_pred = self.unet(
-                sample=noisy_x,
-                timestep=t,
-                encoder_hidden_states=cond
-            ).sample
-            return noise_pred
-
-# Noise scheduler
-scheduler = DDPMScheduler(num_train_timesteps=1000)
+        # forward
+        noise_pred = self.unet(
+            sample=noisy_x,
+            timestep=t,
+            encoder_hidden_states=cond
+        ).sample
+        return noise_pred
