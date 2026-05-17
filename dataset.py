54c8d2378278df8c0b6032d9509a9d2129b8753a
eljandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-05-17 10:47:11 +0200

    build StreamDataset

---
+++ dataset.py
@@ -0,0 +1,81 @@
+from math import ceil
+
+import torch
+from datasets import load_dataset
+from torch.utils.data import IterableDataset, get_worker_info
+from torchvision import transforms
+
+
+class StreamDataset(IterableDataset):
+    def __init__(
+        self,
+        name: str = "immanuelpeter/carla-autopilot-multimodal-dataset",
+        split: str = "train",
+        im_s=256,
+        mean=(0.4738, 0.4824, 0.4592, 1.0000),
+        std=(0.2823, 0.2809, 0.2801, 1e-8),
+    ) -> None:
+        super().__init__()
+        self.main_stream = load_dataset(name, split=split, streaming=True)
+        self._len = self.main_stream.info.splits[split].num_examples  # pyright: ignore[reportOptionalSubscript]
+
+        self.trans = transforms.Compose(
+            [
+                transforms.Resize((im_s, im_s)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=mean, std=std),
+            ]
+        )
+
+    def __len__(self):
+        return self._len
+
+    def _pick(self, sample: dict):
+        data = {}
+        data["x_t"] = self.trans(sample["image_front"])
+        data["a_t"] = torch.tensor(
+            [sample["throttle"], sample["steer"], sample["brake"]]
+        )
+        meta = {}
+        meta["run_id"] = sample["run_id"]
+        meta["frame"] = sample["frame"]
+        return data, meta
+
+    def __iter__(self):
+        worker_info = get_worker_info()
+        if worker_info is None:  # single-process data loading, return the full iterator
+            itb = self.main_stream
+        else:  # in a worker process
+            # split workload
+            per_worker = int(ceil(len(self) / float(worker_info.num_workers)))
+            worker_id = worker_info.id
+            iter_start = worker_id * per_worker
+            chunk = min(per_worker, len(self) - iter_start)
+            itb = self.main_stream.skip(iter_start).take(chunk)
+
+        run_id = ""
+        for it in itb:
+            data, meta = self._pick(it)
+            if run_id != meta["run_id"]:
+                run_id = meta["run_id"]
+                idx = meta["frame"]
+                current_data = data
+                continue
+            assert idx < meta["frame"]
+            idx = meta["frame"]
+            current_data["x_tp1"] = data["x_t"]
+            yield current_data
+            current_data = data
+
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+
+    ds = StreamDataset()
+    k = 4
+    dt = DataLoader(ds, num_workers=k, batch_size=4)
+    for i, s in enumerate(dt):
+        for k, v in s.items():
+            print(i, ";", k, ":", v.shape)
+        if i > 10:
+            break
--- model.py
+++ model.py
@@ -5,72 +5,85 @@ from diffusers.models.unets.unet_2d_condition import UNet2DConditionModel
 
 
 class ActionEmbedder(FeedForward):
-    def __init__(self, action_dim:int=3, embed_dim:int=1024,
-                 num_tokens:int=4, hidden_dim:int=1024,
-                 dropout:float=0.1, act_fn:str="swiglu"):
-        
-        super().__init__(dim=action_dim,  inner_dim=hidden_dim,
-                         dim_out=embed_dim * num_tokens,
-                         activation_fn=act_fn, dropout=dropout)
+    def __init__(
+        self,
+        action_dim: int = 3,
+        embed_dim: int = 1024,
+        num_tokens: int = 4,
+        hidden_dim: int = 1024,
+        dropout: float = 0.1,
+        act_fn: str = "swiglu",
+    ):
+
+        super().__init__(
+            dim=action_dim,
+            inner_dim=hidden_dim,
+            dim_out=embed_dim * num_tokens,
+            activation_fn=act_fn,
+            dropout=dropout,
+        )
 
         self.num_tokens = num_tokens
 
-    def forward(self, a:torch.Tensor):
+    def forward(self, a: torch.Tensor):
         # a: (B, action_dim)
         x = super().forward(a)  # (B, embed_dim * tokens)
         x = x.view(a.shape[0], self.num_tokens, -1)
         return x  # (B, tokens, embed_dim)
-    
+
 
 class WorldModel(nn.Module):
-    def __init__(self, base_name:str="runwayml/stable-diffusion-v1-5",
-                 action_dim:int=3, num_tokens:int=4, hidden_dim:int=1024):
-        
+    def __init__(
+        self,
+        base_name: str = "runwayml/stable-diffusion-v1-5",
+        action_dim: int = 3,
+        num_tokens: int = 4,
+        hidden_dim: int = 1024,
+    ):
+
         super().__init__()
-        self.unet = UNet2DConditionModel.from_pretrained(
-                base_name,
-                subfolder="unet"
-            )
-        
-        self.action_embedder = ActionEmbedder(action_dim=action_dim,
-                                   embed_dim = self.unet.config.cross_attention_dim, # pyright: ignore[reportAttributeAccessIssue]
-                                   num_tokens=num_tokens,
-                                   hidden_dim=hidden_dim,
-                                   )
-        
-    def forward(self, a_t:torch.Tensor, x_t: torch.Tensor):
+        self.unet = UNet2DConditionModel.from_pretrained(base_name, subfolder="unet")
+
+        self.action_embedder = ActionEmbedder(
+            action_dim=action_dim,
+            embed_dim=self.unet.config.cross_attention_dim,  # pyright: ignore[reportAttributeAccessIssue]
+            num_tokens=num_tokens,
+            hidden_dim=hidden_dim,
+        )
+
+    def forward(self, a_t: torch.Tensor, x_t: torch.Tensor):
         cond = self.action_embedder(a_t)  # (B, tokens, dim)
         B = x_t.shape[0]
-        t = torch.ones((B,),dtype=torch.long,device=x_t.device)
+        t = torch.ones((B,), dtype=torch.long, device=x_t.device)
         x_tp1_pred = self.unet(
-            sample=x_t,
-            timestep=t,
-            encoder_hidden_states=cond
+            sample=x_t, timestep=t, encoder_hidden_states=cond
         ).sample
         return x_tp1_pred
-    
+
+
 if __name__ == "__main__":
     from torchvision import transforms
     from tqdm import tqdm
 
     from data_prep import frames
+
     model = WorldModel().cuda()
     im_s = model.unet.config.sample_size * 4
     print(im_s)
-    in_c = model.unet.config.in_channels
     mean = [0.4738, 0.4824, 0.4592, 1.0000]
     std = [0.2823, 0.2809, 0.2801, 1e-8]
-    transform = transforms.Compose([
-        transforms.Resize((im_s, im_s)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std)
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize((im_s, im_s)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]
+    )
     x_t = transform(frames["frame_t"]).unsqueeze(0).cuda()
     x_tp1 = transform(frames["frame_tp1"]).unsqueeze(0).cuda()
     a_t = torch.tensor(frames["action"]).unsqueeze(0).cuda()
     for _ in tqdm(range(1)):
         with torch.inference_mode():
-            x_tp1_pred = model(a_t,x_t)
-            loss = nn.functional.mse_loss(x_tp1_pred,x_tp1)
+            x_tp1_pred = model(a_t, x_t)
+            loss = nn.functional.mse_loss(x_tp1_pred, x_tp1)
             print(loss)
-
